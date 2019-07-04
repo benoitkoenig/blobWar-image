3cc0f74589a61881da1e5200494f7646a6557d58
Benoit Koenig <koenigbenoit47@gmail.com>
2019-07-04 11:25:22 -0600

    Training with a new model. Evaluation give 9152/10000, with label 4 scoring 1039/1717

--- model.py
+++ model.py
@@ -6,17 +6,19 @@ from constants import nb_class
 class BlobClassifierModel(Model):
     def __init__(self):
         super(BlobClassifierModel, self).__init__()
-        self.conv1 = Conv2D(8, kernel_size=(3, 3), strides=(2, 2), activation="relu")
+        self.conv1 = Conv2D(8, kernel_size=(5, 5), activation="relu", padding="same")
+        self.conv2 = Conv2D(8, kernel_size=(3, 3), strides=(2, 2), activation="relu", padding="same")
+        self.conv3 = Conv2D(8, kernel_size=(3, 3), strides=(2, 2), activation="relu", padding="same")
         self.mp1 = MaxPool2D((2, 2))
-        self.conv2 = Conv2D(8, kernel_size=(3, 3), strides=(2, 2), activation="relu")
         self.flat1 = Flatten()
         self.dense1 = Dense(64, activation="relu")
         self.dense_logits = Dense(nb_class, activation="linear")
 
     def call(self, input):
         x = self.conv1(input)
-        x = self.mp1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.mp1(x)
         x = self.flat1(x)
         x = self.dense1(x)
         logits = self.dense_logits(x)
