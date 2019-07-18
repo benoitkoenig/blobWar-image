450ba32fca3c88ccf2a0f6c50a3b8964ec7b67a5
Benoit Koenig <koenigbenoit47@gmail.com>
2019-07-17 22:16:53 -0600

    Add non_max_suppresion

--- yolo/boxes.py
+++ yolo/boxes.py
@@ -1,6 +1,43 @@
 import numpy as np
 
-from constants import feature_size, nb_class, real_image_size
+from constants import feature_size, nb_class, real_image_size, max_boxes, overlap_thresh
+
+def non_max_suppression_fast(input_boxes):
+    boxes = np.copy(input_boxes)
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    area = (x2 - x1) * (y2 - y1)
+
+    picked_boxes = []
+
+    while (len(boxes) > 0) & (len(picked_boxes) < max_boxes):
+        current_box = boxes[0]
+        current_area = area[0]
+        picked_boxes.append(current_box)
+        boxes = boxes[1:]
+        area = area[1:]
+
+        xx1_intersection = np.maximum(boxes[:, 0], current_box[0])
+        yy1_intersection = np.maximum(boxes[:, 1], current_box[1])
+        xx2_intersection = np.minimum(boxes[:, 2], current_box[2])
+        yy2_intersection = np.minimum(boxes[:, 3], current_box[3])
+
+        ww_intersection = np.maximum(0, xx2_intersection - xx1_intersection)
+        hh_intersection = np.maximum(0, yy2_intersection - yy1_intersection)
+
+        area_intersection = ww_intersection * hh_intersection
+
+        area_union = current_area + area - area_intersection
+
+        overlap = area_intersection / area_union # division of ints resulting in a float
+
+        ids_to_delete = np.where(overlap > overlap_thresh)[0]
+        boxes = np.delete(boxes, ids_to_delete, axis=0)
+        area = np.delete(area, ids_to_delete, axis=0)
+
+    return picked_boxes
 
 def get_final_coords(boxes_input):
     boxes = np.copy(boxes_input)
@@ -21,7 +58,16 @@ def get_boxes(preds):
     sorted_boxes_ids = np.argsort(-1 * probs)
     boxes = boxes[sorted_boxes_ids]
 
+    boxes = boxes[boxes[:, 0] < boxes[:, 2]]
+    boxes = boxes[boxes[:, 1] < boxes[:, 3]]
+
+    high_certitude_boxes = np.where(boxes[:, 4] > 10)[0]
+    boxes = boxes[high_certitude_boxes]
+
+    boxes = non_max_suppression_fast(boxes)
+
     boxes = boxes[:6]
+    print(boxes)
     output = []
 
     for b in boxes:
--- yolo/constants.py
+++ yolo/constants.py
@@ -3,3 +3,5 @@ image_size = 256
 real_image_size=(742, 594)
 feature_size = 64
 blob_size = 6
+max_boxes = 18
+overlap_thresh = .2
